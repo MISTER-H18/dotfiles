8c0587fb48be9ee13cad4453bef314a5a255a627
MISTER-H18 <MISTER-H18@users.noreply.github.com>
2024-10-21 12:01:12 -0400

    Upload from Debian GNU/Linux 12

---
+++ .scripts/goodbye.py
@@ -0,0 +1,11 @@
+def goodbye() -> None:
+    print("\n")
+    print("          |\      _,,,---,,_     ")
+    print("    ZZZzz /,`.-'`'    -.  ;-;;,  ")
+    print("         |,4-  ) )-,_. ,\ (  `'-'")
+    print("        '---''(_/--'  `-'\_)     ")
+    print("\n")
+    print("                GOODBYE!!            ")
+    print("\n")
+
+goodbye()
